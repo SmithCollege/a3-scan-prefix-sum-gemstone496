4f3473ba22e88603792ffb6aad8e48e6645cee29
Jade Lilian <lilianpalosky@gmail.com>
2024-11-07 10:32:41 -0500

    fail to use RStudio---pausing for future implementations of seaborn

--- render_data.py
+++ render_data.py
@@ -7,9 +7,10 @@ sns.set_theme()
 
 # Load data
 PATH = "timers/"
-cpuTime = pd.read_csv(PATH + "cpu.csv", header=0, index_col=0)
-naiveTime = pd.read_csv(PATH + "naive.csv", header=0, index_col=0)
-mkTime = pd.read_csv(PATH + "mkernel.csv", header=0, index_col=0)
+cpuTime = pd.read_csv(PATH + "cpu.csv", header=0)
+naiveTime = pd.read_csv(PATH + "naive.csv", header=0)
+mkTime = pd.read_csv(PATH + "mkernel.csv", header=0)
 
 # Pivots
-cpuTime = pd.wide_to_long(cpuTime, id_vars=0)
+cpuTime = pd.wide_to_long(cpuTime, stubnames="", i=size)
+
